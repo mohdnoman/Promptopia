e0d4aec28b080ad311a86233461658a508b920a2
mohd noman <mohdnoman2751@gmail.com>
2024-02-08 18:07:03 +0530

    made minor changes for deployement.

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -6,7 +6,7 @@ import Form from "@components/Form";
 
 const EditPrompt = () => {
   const router = useRouter();
-  const searchParams = useSearchParams();
+  const searchParams = useSuspenseWithSearchParams();
   const promptId = searchParams.get("id");
 
   const [submitting, setSubmitting] = useState(false);
